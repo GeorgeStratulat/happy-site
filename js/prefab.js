96c4b9f7576f339b01ca49222708017ca7c57a94
Iancu George-Alexandru <alex1gtp@gmail.com>
2018-09-27 09:17:24 +0300

    some changes on files

--- js/prefab.js
+++ js/prefab.js
@@ -1,3 +1,10 @@
+// Template: BOOZE. - Template HTML5 / CSS3
+// Author: alexiancu.com
+// Version: 1.0.0
+// Design and Developed by: Alex Iancu - Web & App Developer
+
+// NOTE:  
+
 
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
